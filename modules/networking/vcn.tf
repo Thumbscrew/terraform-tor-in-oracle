564ef63e47c4a2d72d1d0875688c00d67ccb5bbf
James <thumbscrw@pm.me>
2022-04-17 17:28:46 +0100

    add igw and routing

--- modules/networking/vcn.tf
+++ modules/networking/vcn.tf
@@ -3,3 +3,13 @@ resource "oci_core_vcn" "vcn" {
   compartment_id = var.compartment_ocid
   display_name   = var.vcn_display_name
 }
+
+resource "oci_core_default_dhcp_options" "default_dhcp_options" {
+  manage_default_resource_id = oci_core_vcn.vcn.default_dhcp_options_id
+  display_name               = "defaultDhcpOptions"
+
+  options {
+    type        = "DomainNameServer"
+    server_type = "VcnLocalPlusInternet"
+  }
+}
